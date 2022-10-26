d9708d42fe3e6705807cb81ffe06c401d93e5bf3
Kim Jae Hun <wogns1733@gmail.com>
2022-10-26 09:33:35 +0900

    [Bronze V] Title: A-B, Time: 64 ms, Memory: 29160 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.py
@@ -0,0 +1,12 @@
+def scan_input():
+    temp = input()
+    arr = temp.split(" ")
+    return arr
+
+scanArr = scan_input()
+A, B = int( scanArr[0] ), int( scanArr[1] )
+
+if 0 < A < 10 and 0 < B < 10:
+    print(A-B)
+else:
+    print("error")
