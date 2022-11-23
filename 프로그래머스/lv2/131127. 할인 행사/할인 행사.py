f7ebf5d30989ee1e1c0ff45ef48797d6ec71c26e
Kim Jae Hun <wogns1733@gmail.com>
2022-11-23 16:18:48 +0900

    [level 2] Title: 할인 행사, Time: 0.05 ms, Memory: 9.96 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
@@ -0,0 +1,24 @@
+from collections import Counter
+
+def solution(want, number, discount):
+    answer = 0
+    goal = {}
+    
+    # 가격 매칭
+    for i in range(len(want)):
+        goal[want[i]] = number[i]
+    
+    # print(goal)
+    for i in range(len(discount)):
+        product = Counter(discount[i:min(i+10, len(discount))])
+        flag = 1
+        for key in goal.keys():
+            if (key not in product) or (product[key] < goal[key]):
+                # print(product, key)
+                flag = 0
+                break
+        
+        if flag:
+            answer += 1
+            
+    return answer
