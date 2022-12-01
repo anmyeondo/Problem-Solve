f4ac3c80419982c2cad5540b92e1104591994530
Kim Jae Hun <wogns1733@gmail.com>
2022-12-01 16:07:08 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 10.15 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -0,0 +1,24 @@
+def binary(s):
+    zero = 0
+    one = 0
+    
+    for bit in s:
+        if bit == '0':
+            zero += 1
+        else:
+            one += 1
+    
+    return [zero, bin(one)[2:]]
+
+def solution(s):
+    answer = [0, 0]
+    
+    answer[0] += 1
+    answer[1], s = binary(s)
+    
+    while s != "1":
+        zero, s = binary(s)
+        answer[0] += 1
+        answer[1] += zero
+        
+    return answer
